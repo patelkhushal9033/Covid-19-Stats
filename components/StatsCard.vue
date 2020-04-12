b1feb498f670b8ebde5df1fb338619890c6e304e
Khushal <patelkhushal9033@gmail.com>
2020-04-12 16:24:32 +1000

    tweaked css statsCard

--- components/StatsCard.vue
+++ components/StatsCard.vue
@@ -5,23 +5,23 @@
           class="statsCard"
           style="display: flex; overflow-x: auto">
           <v-card 
-            class="moreInfo" color="#b33939">
+            class="moreInfo white--text" color="#b33939">
             <div class="text-center body-2">Deaths</div>
             <div class="text-center headline">12045</div>
             <div class="text-center caption">+ 321 today</div>
           </v-card>
           <v-card 
-            class="moreInfo" color="#218c74">
+            class="moreInfo white--text" color="#218c74">
             <div class="text-center body-2 mb-2">Recovered</div>
             <div class="text-center headline">80567</div>
           </v-card>
           <v-card 
-            class="moreInfo" color="#cd6133">
+            class="moreInfo white--text" color="#cd6133">
             <div class="text-center body-2 mb-2">Active</div>
             <div class="text-center headline">80787</div>
           </v-card>
           <v-card 
-            class="moreInfo">
+            class="moreInfo white--text" color="#cc8e3">
             <div class="text-center body-2 mb-2">Critical</div>
             <div class="text-center headline">5678</div>
           </v-card>
